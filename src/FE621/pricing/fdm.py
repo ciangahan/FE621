289c48512b206aa8c730a3f82a3edd063bea33f4
ciangahan <cgahan13@gmail.com>
2026-03-13 21:48:13 -0400

    implicit fd method

--- src/FE621/pricing/fdm.py
+++ src/FE621/pricing/fdm.py
@@ -66,22 +66,173 @@ class ImplicitFD:
 
     O(∆x^2 + ∆t) convergence
     """
-    pass
+    @staticmethod
+    def price_option(S0, K, r, div, sigma, T, N, Nj, dx, call:bool):
+        """
+        Price option via Implicit Finite Difference Method
+        @param S0: initial stock price
+        @param K: strike
+        @param r: risk free rate (log)
+        @param div: dividend rate (log)
+        @param sigma: stock volatility
+        @param T: time to maturity
+        @param N: Number of time steps
+        @param Nj: Number of log stock price steps
+        @param dx: Log stock price step size
+        """
+        dt = T/N
+        mu = r - div - 0.5 * (sigma ** 2)
+        edx = np.exp(dx)
+
+        pu = -0.5 * dt * ((sigma / dx) ** 2 + mu / dx)
+        pm = 1 + dt * (sigma / dx) ** 2 + r * dt
+        pd = -0.5 * dt * ((sigma / dx) ** 2 - mu / dx)
+
+        stock_prices = np.zeros(2 * Nj + 1)
+        stock_prices[-1] = S0 * np.exp(-Nj * dx)
+        for j in reversed(range(2 * Nj)):
+            stock_prices[j] = stock_prices[j+1] * edx
+
+        price_grid = np.zeros((2 * Nj + 1, N + 1))
+
+        # terminal payoff
+        if call:
+            for j in range(2 * Nj + 1):
+                price_grid[j, N] = max(stock_prices[j] - K, 0)
+            lambda_U = stock_prices[0] - stock_prices[1]
+            lambda_L = 0
+        else:
+            for j in range(2 * Nj + 1):
+                price_grid[j, N] = max(K - stock_prices[j], 0)
+            lambda_U = 0
+            lambda_L = stock_prices[-1] - stock_prices[-2]
+        
+        for i in reversed(range(N)):
+            ImplicitFD.solve_implicit_tridiagonal_system(price_grid, i, pu, pm, pd, lambda_L, lambda_U, Nj)
+
+            if call:
+                for j in range(2 * Nj + 1):
+                    price_grid[j, i] = max(price_grid[j, i], max(stock_prices[j] - K, 0))
+            else:
+                for j in range(2 * Nj + 1):
+                    price_grid[j, i] = max(price_grid[j, i], max(K - stock_prices[j], 0))
+
+        return price_grid[Nj, 0]
+
+
+    @staticmethod
+    def solve_implicit_tridiagonal_system(price_grid, i, pu, pm, pd, lambda_L, lambda_U, Nj):
+        # pm primes, p primes when subsitituting up 
+        pmp = np.zeros(2 * Nj + 1)
+        pp = np.zeros(2 * Nj + 1)
+
+        pmp[2 * Nj - 1] = pm + pd
+        pp[2 * Nj - 1] = price_grid[2 * Nj - 1, i + 1] + pd * lambda_L
+
+        for j in np.arange(2 * Nj - 2, 0, -1):
+            pmp[j] = pm - pu * pd / pmp[j + 1]
+            pp[j] = price_grid[j, i + 1] - pp[j + 1] * pd/pmp[j + 1]
+
+        price_grid[0, i] = (pp[1] + pmp[1] * lambda_U) / (pu + pmp[1])
+        price_grid[1, i] = price_grid[0, i] - lambda_U
+        
+        for j in range(2, 2 * Nj):
+            price_grid[j, i] = (pp[j] - pu * price_grid[j-1, i]) / pmp[j]
+        
+        price_grid[2 * Nj, i] = price_grid[2 * Nj - 1, i] - lambda_L
+
+        return
 
 
 class CrankNicolsonFD:
     """
     Crank-Nicolson Finite Difference Method
     """
-    pass
+    @staticmethod
+    def price_option(S0, K, r, div, sigma, T, N, Nj, dx, call:bool):
+        """
+        Price option via Crank-Nicolson Finite Difference Method
+        @param S0: initial stock price
+        @param K: strike
+        @param r: risk free rate (log)
+        @param div: dividend rate (log)
+        @param sigma: stock volatility
+        @param T: time to maturity
+        @param N: Number of time steps
+        @param Nj: Number of log stock price steps
+        @param dx: Log stock price step size
+        """
+        dt = T/N
+        mu = r - div - 0.5 * (sigma ** 2)
+        edx = np.exp(dx)
+
+        pu = -0.5 * dt * ((sigma / dx) ** 2 + mu / dx)
+        pm = 1 + dt * (sigma / dx) ** 2 + r * dt
+        pd = -0.5 * dt * ((sigma / dx) ** 2 - mu / dx)
+
+        stock_prices = np.zeros(2 * Nj + 1)
+        stock_prices[-1] = S0 * np.exp(-Nj * dx)
+        for j in reversed(range(2 * Nj)):
+            stock_prices[j] = stock_prices[j+1] * edx
+
+        price_grid = np.zeros((2 * Nj + 1, N + 1))
+
+        # terminal payoff
+        if call:
+            for j in range(2 * Nj + 1):
+                price_grid[j, N] = max(stock_prices[j] - K, 0)
+            lambda_U = stock_prices[0] - stock_prices[1]
+            lambda_L = 0
+        else:
+            for j in range(2 * Nj + 1):
+                price_grid[j, N] = max(K - stock_prices[j], 0)
+            lambda_U = 0
+            lambda_L = stock_prices[-1] - stock_prices[-2]
+        
+        for i in reversed(range(N)):
+            ImplicitFD.solve_implicit_tridiagonal_system(price_grid, i, pu, pm, pd, lambda_L, lambda_U, Nj)
+
+            if call:
+                for j in range(2 * Nj + 1):
+                    price_grid[j, i] = max(price_grid[j, i], max(stock_prices[j] - K, 0))
+            else:
+                for j in range(2 * Nj + 1):
+                    price_grid[j, i] = max(price_grid[j, i], max(K - stock_prices[j], 0))
+
+        return price_grid[Nj, 0]
+
+
+    @staticmethod
+    def solve_implicit_tridiagonal_system(price_grid, i, pu, pm, pd, lambda_L, lambda_U, Nj):
+        # pm primes, p primes when subsitituting up 
+        pmp = np.zeros(2 * Nj + 1)
+        pp = np.zeros(2 * Nj + 1)
+
+        pmp[2 * Nj - 1] = pm + pd
+        pp[2 * Nj - 1] = price_grid[2 * Nj - 1, i + 1] + pd * lambda_L
+
+        for j in np.arange(2 * Nj - 2, 0, -1):
+            pmp[j] = pm - pu * pd / pmp[j + 1]
+            pp[j] = price_grid[j, i + 1] - pp[j + 1] * pd/pmp[j + 1]
+
+        price_grid[0, i] = (pp[1] + pmp[1] * lambda_U) / (pu + pmp[1])
+        price_grid[1, i] = price_grid[0, i] - lambda_U
+        
+        for j in range(2, 2 * Nj):
+            price_grid[j, i] = (pp[j] - pu * price_grid[j-1, i]) / pmp[j]
+        
+        price_grid[2 * Nj, i] = price_grid[2 * Nj - 1, i] - lambda_L
+
+        return
 
 
 if __name__ == "__main__":
     S0 = 100
-    K = 105
-    sigma = 0.25
-    r = 0.05
+    K = 100
+    sigma = 0.2
+    r = 0.06
     T = 1
+    div = 0.00
     steps = 1000
 
     tree = TrinomialTree(r, sigma, S0, T, steps)
@@ -90,3 +241,6 @@ if __name__ == "__main__":
 
     efd = ExplicitFD(r, sigma, S0, T, steps)
     print(efd.price_option(K, call=True))
+
+
+    print(ImplicitFD.price_option(S0, K, r, div, sigma, T, N=1000, Nj=1000, dx=0.002, call=True))
